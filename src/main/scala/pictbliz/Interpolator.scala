ad6624125627613a4eb01097dd58a8b4e2d38178
whelmaze <whelmaze@gmail.com>
2015-10-11 18:37:23 +0900

    Interpolator can accept other Values now.
    Fix unnecessary extra recursion.
    Fix deep nest like Hadouken.

--- src/main/scala/pictbliz/Interpolator.scala
+++ src/main/scala/pictbliz/Interpolator.scala
@@ -28,26 +28,49 @@ class Interpolator(target: Layouts.VMap, ids: Seq[Int] = Vector(0)) extends Lazy
 
   final val reg = """\#\{(\w+)\}""".r
 
+  // Rule of convert value to string while interpolation.
+  private[this] def value2String(v: Value): String = v match {
+    case Text(s) => s
+    case Icon(s) => s
+    case FaceGraphic(s, _, _) => s
+    case CharaGraphic(s, _, _) => s
+    case BattleGraphic(s, _, _) => s
+    case _ => ""
+  }
+
   // Return interpolated Values.Value with 'id'.
   def interpolateWithId(id: Int, key: String, value: Values.Value, visited: Set[String] = Set.empty[String]): Values.Value = {
+
+    def impl(self: Value, str: String, makeSelf: String => Value): Value = {
+      val newTo = reg.findFirstMatchIn(str).map { m =>
+        val name = m.group(m.groupCount)
+        val replaceTo = interpolateWithId(id, name, target(name), visited + key)
+        val p = replaceTo match {
+          case Text(rep) => rep
+          case etc => value2String(etc)
+        }
+        makeSelf(str.replace(m.matched, p))
+      }
+      newTo.map(
+        // continuing interpolate next #{}.
+        interpolateWithId(id, key, _, visited)
+      ) getOrElse self
+    }
+
+    // if detect circular reference error, all values are convert to Values.Text with error msg.
     if (visited(key)) {
       val msg = "Circular Reference Error Occurred."
-      //logger.error(msg + s" id: $id, key: $key, value: $value, visited: $visited")
+      //logger.error(msg + s" id: $id, key: $key, value: $value")
       Text(msg)
     } else {
+
+      // ugly...
       value match {
-        case t @ Text(str) =>
-          reg.findFirstMatchIn(str).map { m =>
-            val name = m.group(m.groupCount)
-            val replaceTo = interpolateWithId(id, name, target(name), visited + key)
-            replaceTo match {
-              case Text(rep) =>
-                val newTo = Text(str.replace(m.matched, rep))
-                // continuing interpolate next #{}.
-                interpolateWithId(id, name, newTo, visited)
-              case etc => etc
-            }
-          } getOrElse t
+        case t @ Text(str) => impl(t, str, s => t.copy(str = s))
+        case i @ Icon(str) => impl(i, str, s => i.copy(path = s))
+        case f @ FaceGraphic(str, _, _) => impl(f, str, s => f.copy(path = s))
+        case c @ CharaGraphic(str, _, _) => impl(c, str, s => c.copy(path = s))
+        case b @ BattleGraphic(str, _, _) => impl(b, str, s => b.copy(path = s))
         case etc => etc
       }
     }
--- src/test/scala/pictbliz/InterpolatorSpec.scala
+++ src/test/scala/pictbliz/InterpolatorSpec.scala
@@ -16,7 +16,7 @@ class InterpolatorSpec extends UnitSpec {
       res("id") should equal (Text("0"))
     }
 
-    "interpolate correctly single #{} string" in {
+    "interpolate single #{} string" in {
       val res = new Interpolator(
         Map("test" -> Text("test"),
             "inp" -> Text("interpolated: #{test}"))).interpolate(0)
@@ -24,7 +24,7 @@ class InterpolatorSpec extends UnitSpec {
       res("inp") should equal (Text("interpolated: test"))
     }
 
-    "interpolate correctly twice nested #{} string" in {
+    "interpolate twice nested #{} string" in {
       val res = new Interpolator(
         Map("test" -> Text("test"),
             "inp1" -> Text("interpolated: #{test}"),
@@ -42,7 +42,7 @@ class InterpolatorSpec extends UnitSpec {
       res("B") should equal (CREText)
     }
 
-    "interpolate correctly multiple #{} string" in {
+    "interpolate multiple #{} string" in {
       val res = new Interpolator(
         Map("A" -> Text("the Answer to The Ultimate Question of Life, the Universe, and Everything"),
             "B" -> Text("42"),
@@ -59,6 +59,27 @@ class InterpolatorSpec extends UnitSpec {
       } toMap
       val res = new Interpolator(map).interpolate(0)
       res("1") should equal (CREText)
+    }
+
+    "interpolate Values not Text" in {
+      val res = new Interpolator(
+        Map("A" -> Text("drill.png"),
+            "B" -> Icon("#{A}"),
+            "C" -> FaceGraphic("#{A}", 0, true),
+            "D" -> CharaGraphic("#{A}", CharaProperty(0, 0, 0), true),
+            "E" -> BattleGraphic("#{A}", 0, true)
+            )).interpolate(0)
+      res("B") should equal (Icon("drill.png"))
+      res("C") should equal (FaceGraphic("drill.png", 0, true))
+      res("D") should equal (CharaGraphic("drill.png", CharaProperty(0, 0, 0), true))
+      res("E") should equal (BattleGraphic("drill.png", 0, true))
+    }
+
+    "interpolate Values not Text with path" in {
+      val res = new Interpolator(
+        Map("A" -> Text("B's path is #{B}"),
+            "B" -> Icon("path.png"))).interpolate(0)
+      res("A") should equal (Text("B's path is path.png"))
     }
 
   }
