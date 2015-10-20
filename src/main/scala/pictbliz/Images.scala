26daa798644d630e8aa9eb641ea6a9bad9f86f1f
whelmaze <whelmaze@gmail.com>
2015-10-20 10:43:00 +0900

    Use default name if VMap has no "filename" key.

--- src/main/scala/pictbliz/Images.scala
+++ src/main/scala/pictbliz/Images.scala
@@ -3,6 +3,8 @@ package pictbliz
 import java.awt.Color
 import java.awt.image.BufferedImage
 
+import com.github.nscala_time.time.StaticDateTimeFormat
+
 import scalaz.Semigroup
 
 object Images {
@@ -47,11 +49,18 @@ case class ImageResult(filename: String, image: BufferedImage) {
 }
 
 case class ImagePart(pos: (Int, Int), image: BufferedImage) {
+  import com.github.nscala_time.time.Imports._
+
+  private[this] final val fmt = StaticDateTimeFormat.forPattern("yyyyMMddHHmmssSSS")
+
+  def genRandomName: String = DateTime.now().toString(fmt) + util.Random.alphanumeric.take(6).mkString
+
+  def trimPath(s: String): String = s.replaceAll("""\.png$""", "")
 
   def result(values: Map[Layouts.Id, Values.Value]): ImageResult = {
     val filename = values.get("filename") match {
-      case Some(Values.Text(str)) => str
-      case _ => "untitled"
+      case Some(Values.Text(str)) => trimPath(str)
+      case _ => genRandomName
     }
     ImageResult(filename, image)
   }
